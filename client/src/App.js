770d51b32ea3b6d2baf713b1c324bb8fc9ed7f2c
Chuck Hagy <chuck.hagy@liveramp.com>
2019-04-11 11:22:45 -0700

    sanity check for deployment

--- client/src/App.js
+++ client/src/App.js
@@ -7,22 +7,23 @@ import Fib from './Fib';
 
 class App extends Component {
   render() {
-    return (
-      <Router>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-            <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
-          </header>
-          <div>
-            <Route exact path="/" component={Fib} />
-            <Route path="/otherpage" component={OtherPage} />
-          </div>
-        </div>
-      </Router>
-    );
+    <div>Hello there</div>;
+    // return (
+    //   <Router>
+    //     <div className="App">
+    //       <header className="App-header">
+    //         <img src={logo} className="App-logo" alt="logo" />
+    //         <h1 className="App-title">Welcome to React</h1>
+    //         <Link to="/">Home</Link>
+    //         <Link to="/otherpage">Other Page</Link>
+    //       </header>
+    //       <div>
+    //         <Route exact path="/" component={Fib} />
+    //         <Route path="/otherpage" component={OtherPage} />
+    //       </div>
+    //     </div>
+    //   </Router>
+    // );
   }
 }
 
